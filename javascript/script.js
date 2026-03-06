0c237b26bddc1e0e70d72bd19c97280f0961e801
Roohuleman <www.emanpervaiz@gmail.com>
2026-03-06 23:52:28 +0500

    javascript web task 2

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    console.log("Button clicked!");
+    if (document.body.classList.contains("dark-mode")) {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
+    } else {
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("dark-mode");
+    }
+})
